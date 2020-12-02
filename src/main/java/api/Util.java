eac8e189292d9092605299f0eebdec05e05c9811
Dyrhoi <realarcoz@gmail.com>
2020-12-02 09:42:16 +0100

    Validation and Order Initialization.

---
+++ src/main/java/api/Util.java
@@ -0,0 +1,15 @@
+package api;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class Util {
+    private String generateSecureToken() {
+        Base64.Encoder encoder = Base64.getUrlEncoder().withoutPadding();
+        SecureRandom random = new SecureRandom();
+
+        byte[] bytes = new byte[24];
+        random.nextBytes(bytes);
+        return encoder.encodeToString(bytes);
+    }
+}
