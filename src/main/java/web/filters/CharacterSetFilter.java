c63135bfa151220ffbd3c547c1053f2a1650e1b8
Dyrhoi <realarcoz@gmail.com>
2020-12-07 15:46:40 +0100

    Removed logging... Filter is filtering, feelsOkay

--- src/main/java/web/filters/CharacterSetFilter.java
+++ src/main/java/web/filters/CharacterSetFilter.java
@@ -9,9 +9,13 @@ public class CharacterSetFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
-        /*response.setContentType("text/html; charset=UTF-8");
-        response.setCharacterEncoding("UTF-8");*/
+        /*
+        response.setContentType("text/html; charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        */
         filterChain.doFilter(request, response);
+        /*
         LoggerFactory.getLogger(CharacterSetFilter.class).info(request.getParameter("first-name"));
+        */
     }
 }
