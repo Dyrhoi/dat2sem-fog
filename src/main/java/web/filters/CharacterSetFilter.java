fbfa0341ff8eb3e1785307af2e2607b265a76175
Dyrhoi <realarcoz@gmail.com>
2020-12-07 15:28:05 +0100

    Filter changes.

--- src/main/java/web/filters/CharacterSetFilter.java
+++ src/main/java/web/filters/CharacterSetFilter.java
@@ -1,5 +1,7 @@
 package web.filters;
 
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.*;
 import java.io.IOException;
 
@@ -7,8 +9,9 @@ public class CharacterSetFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-        response.setCharacterEncoding("UTF-8");
+        /*response.setContentType("text/html; charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");*/
         filterChain.doFilter(request, response);
+        LoggerFactory.getLogger(CharacterSetFilter.class).info(request.getParameter("first-name"));
     }
 }
