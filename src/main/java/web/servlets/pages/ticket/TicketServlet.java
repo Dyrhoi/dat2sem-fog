77b7d7a560d8997150e66c2191accc2509bea80f
Dyrhoi <realarcoz@gmail.com>
2020-12-09 10:03:08 +0100

    Back-end setup.

--- src/main/java/web/servlets/pages/ticket/TicketServlet.java
+++ src/main/java/web/servlets/pages/ticket/TicketServlet.java
@@ -2,8 +2,20 @@ package web.servlets.pages.ticket;
 
 import web.servlets.BaseServlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet({"/ticket", "/ticket/*"})
 public class TicketServlet extends BaseServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getPathInfo() != null && req.getPathInfo().length() > 1) {
+            String slug = req.getPathInfo().substring(1);
+
+
+        }
+    }
 }
