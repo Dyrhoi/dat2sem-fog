c0f440b0f8c26d5275bd78a7547e2f3f4d630a52
Dyrhoi <realarcoz@gmail.com>
2020-12-09 09:52:25 +0100

    Initial Ticket Servlet

---
+++ src/main/java/web/servlets/pages/ticket/TicketServlet.java
@@ -0,0 +1,9 @@
+package web.servlets.pages.ticket;
+
+import web.servlets.BaseServlet;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet({"/ticket", "/ticket/*"})
+public class TicketServlet extends BaseServlet {
+}
