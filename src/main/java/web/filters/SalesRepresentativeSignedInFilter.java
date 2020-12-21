0c63091963b650fd38bb8d56d5e1fbe83a86d48c
Dyrhoi <realarcoz@gmail.com>
2020-12-21 12:10:34 +0100

    Rename filter.

--- src/main/java/web/filters/SalesRepresentativeSignedInFilter.java
+++ src/main/java/web/filters/SalesRepresentativeSignedInFilter.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class SignedInFilter implements Filter {
+public class SalesRepresentativeSignedInFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
